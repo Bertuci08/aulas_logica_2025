c8e74fe133d56916d4d377a95e52d6882624cd00
Gabriel Bertuci <g.d'avila@aluno.senai.br>
2025-04-08 10:50:49 -0300

    Branch

--- git.js
+++ git.js
@@ -3,5 +3,7 @@ console.log("Todas as comidas", comidas);
 
 
 console.log('Todas as comidas', comidas);
+console.log('Todas as comidas', comidas);
+console.log('Teste mesclar branch');
 comidas.sort()
 console.log('Comidas ordenadas', comidas);
