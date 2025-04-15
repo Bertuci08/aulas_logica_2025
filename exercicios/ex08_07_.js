32cfbf899cec68690bfd968908f2b3f3e2a8e3cd
Gabriel Bertuci <g.d'avila@aluno.senai.br>
2025-04-15 09:07:35 -0300

    Exercicios 08

--- exercicios/ex08_07_.js
+++ exercicios/ex08_07_.js
@@ -1,7 +1,7 @@
 //Nessa string ‘Gol; Corsa; Palio; Monza; Fusca;’, faça a separação desses itens para uma lista e exiba o item e a posição que ele está.
-let carros = 'Gol, Corsa, Palio, Monza, Fusca'
+let carros = 'Gol,Corsa,Palio,Monza,Fusca'
 
 let listaCarros = carros.split(',')
-for(let carros of listaCarros){
-    console.log(carros);
+for(let[posicao, carro] of listaCarros.entries()){
+    console.log(`Poisção ${posicao}: ${carro}`);
 }
